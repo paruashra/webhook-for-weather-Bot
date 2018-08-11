665c68d3da43d3d34f72d334a7e5a7238139bd4a
paruashra <paruashra@gmail.com>
2018-08-11 21:15:51 +0530

    cd

--- webhook.py
+++ webhook.py
@@ -6,46 +6,50 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhookget', methods =['GET']
+def webGet():
+    return "Hello World"
+@app.route('/webhook', methods =['POST']
 def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
-
+    res = makeresponse(req)
     res = json.dumps(res, indent=4)
-    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def makeResponse(req):
-    if req.get("result").get("action") != "fetchWeatherForecast":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    date = parameters.get("date")
-    if city is None:
-        return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=988f06f4fdfda98d437198145dbfe7b5')
+
+def makeresponse(req):
+    result = req.get('queryResult')
+    param = result.get('parameters')
+    dateperiod = param.get('date-period')
+    date = param.get('date')
+    city = param.get('geo-city')
+
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=988f06f4fdfda98d437198145dbfe7b5')
     json_object = r.json()
-    weather=json_object['list']
-    for i in range(0,30):
+    weather = json_object['list']
+    for i in range(0, len(weather)):
         if date in weather[i]['dt_txt']:
-            condition= weather[i]['weather'][0]['description']
-            break
-    speech = "The forecast for"+city+ "for "+date+" is "+condition
-    return {
-    "speech": speech,
-    "displayText": speech,
-    "source": "apiai-weather-webhook"
-    }
+            condition = weather[i]['weather'][0]['description']
+
+    print('Condition  ',condition)
+    speech = "The forecast for "+city+" for date "+date+"is "+condition
+    return         "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            speech
+          ]
+        }
+      }
+    ]
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+    print('starting app om port ', port)
+    app.run(debug=True, port=port, host='0.0.0.0')
